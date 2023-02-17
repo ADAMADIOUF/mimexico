c6969a14e531ec549a233c9f15e17b10cca6a9ce
adama diouf <adamadiouf2017@gmail.com>
2023-02-17 16:30:29 -0600

    change

--- src/Submenu.js
+++ src/Submenu.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { useGlobalContext } from './context'
 import { Link } from 'react-router-dom'
+import { useGlobalContext } from './context'
+
 const Submenu = () => {
   const {
     isSubmenuOpen,
