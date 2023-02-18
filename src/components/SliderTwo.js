1aee0a1e835c0a779387685b152a2bca45bb827d
adama diouf <adamadiouf2017@gmail.com>
2023-02-18 17:21:14 -0600

    change

--- src/components/SliderTwo.js
+++ src/components/SliderTwo.js
@@ -18,9 +18,36 @@ export default class SimpleSlider extends Component {
       slidesToShow: 3,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 2000,
+      autoplaySpeed: 4000,
       pauseOnHover: true,
       cssEase: 'linear',
+
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
     }
     return (
       <div className='slider-tow'>
